1ed54fe2a1bde95c0ff19548b5f33d62b031b395
Ibrahim Elnahal <ibrahimelnahal20@gmail.com>
2025-10-08 18:16:22 +0300

    Fix: base href for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7fccbdb18d7437116139447d5a6c96f0
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "1d6e279a50a0161fba920a834ec729a5",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "1cdd9427450162d8a0b456dc924ecec7",
+"flutter_bootstrap.js": "68ecc58a871a7055e627d4d8bc440621",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
